29d1757b073555ac7e8860a44acb136a4f66f015
Amal Prasad <amalprasad354@gmail.com>
2021-12-08 21:00:24 +0530

    partials and admin/user panel set

--- app.js
+++ app.js
@@ -3,16 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var hbs = require('hbs');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var hbs=require('express-handlebars');
+const { create } = require("express-handlebars");
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.engine('hbs',hbs({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/layout/',partialsDir:__dirname+'views/partials/'}));
+/*app.engine('hbs', hbs({extname: 'hbs', defualtLayout : 'layout' , layoutsDir: __dirname + '/views/layouts',partialsDir:__dirname+'/views/partials/'}));*/
+// new code edited//
+const hbs = create({
+  layoutsDir: `${__dirname}/views/layout`,
+  extname: `hbs`,
+  defaultLayout: 'layout',
+  partialsDir: `${__dirname}/views/partials`
+});
+app.engine('hbs', hbs.engine);
+// ended//
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,12 +31,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
