d7e5d4e44724d269b915f0252d0f29fef25bc2ea
Amal Prasad <amalprasad354@gmail.com>
2021-12-09 21:37:03 +0530

    image upload bug fixed

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var userRouter = require('./routes/user');
+var adminRouter = require('./routes/admin');
 const { create } = require("express-handlebars");
 var app = express();
-
+var fileUpload=require('express-fileupload')
+const bodyParser = require('body-parser')
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -26,9 +27,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileUpload())
+// body praser added
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : true}))
+// endeed
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.use('/', userRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
