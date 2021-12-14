649a77bde402e345526dcca68da4550ead5bad70
Amal Prasad <amalprasad354@gmail.com>
2021-12-14 22:39:27 +0530

    data retrived

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,12 +1,25 @@
 var db=require('../config/connection')
+var BSON = require('mongodb').BSONPure;
+var collection=require('../config/collections')
 module.exports={
     addProduct:(product,callback)=>{
         console.log(product);
 
         db.get().collection('product').insertOne(product).then((data)=>{
-            console.log(data);
-            callback(data.ops[0]._id)
+            console.log(data.insertedId);
+            callback(data.insertedId)
             
+        })
+    },
+    getAllProducts:()=>
+    {
+        return new Promise(async(resolve,reject)=>{
+            let products=await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+            resolve(products)
         })
     }
 }
+
+
+
+
